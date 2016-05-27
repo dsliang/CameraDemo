75d2be5ddb34cc43df00cafea289e24524f971d7
dsliang <zq2114522@163.com>
2016-05-27 08:45:15 +0800

    Camera Demo

---
+++ app/src/main/java/com/tuodi/library/camerademo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tuodi.library.camerademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
